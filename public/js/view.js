bb2589bf058d07984e7d66dbcb00f5439f91636d
Admin <mercerbe@gmail.com>
2018-06-25 14:50:59 -0400

    mvp: needs- style timeleft display, deploy

--- public/js/view.js
+++ public/js/view.js
@@ -135,20 +135,81 @@ $(document).ready(function() {
 
   //----------------------------clock--------------------------------------//
   const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
-function setDate() {
-  const now = new Date();
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-  const hour = now.getHours();
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-}
-setInterval(setDate, 1000);
-setDate();
+  const minsHand = document.querySelector('.min-hand');
+  const hourHand = document.querySelector('.hour-hand');
+
+  function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  }
+  setInterval(setDate, 1000);
+  setDate();
+
+  //-----------------------------timer--------------------------------------//
+  let countdown;
+  const timerDisplay = document.querySelector('.display__time-left');
+  const endTime = document.querySelector('.display__end-time');
+  const buttons = document.querySelectorAll('[data-time]');
+
+  function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    displayEndTime(then);
+
+    countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      // check if we should stop it!
+      if (secondsLeft < 0) {
+        clearInterval(countdown);
+        return;
+      }
+      // display it
+      displayTimeLeft(secondsLeft);
+    }, 1000);
+  }
+
+  function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+    document.title = display;
+    timerDisplay.textContent = display;
+  }
+
+  function displayEndTime(timestamp) {
+    const end = new Date(timestamp);
+    const hour = end.getHours();
+    const adjustedHour = hour > 12 ? hour - 12 : hour;
+    const minutes = end.getMinutes();
+    endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  }
+
+  function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+  }
+
+  buttons.forEach(button => button.addEventListener('click', startTimer));
+  document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
+  });
+
+
+
 });
